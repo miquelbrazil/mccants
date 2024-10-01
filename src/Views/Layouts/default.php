aed094f030e5740099593563d33d5858f5ded1a4
Miquel Brazil <miquel@brazilliance.co>
2024-09-30 20:28:50 -0400

    task: Add favicons and web manifest for better site branding
    Introduced various favicon files and a site web manifest to improve the website's branding and user experience across multiple devices. Updated default layout to include links to the new icons and manifest file, and set meta tags with relevant theme colors.

--- src/Views/Layouts/default.php
+++ src/Views/Layouts/default.php
@@ -5,6 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="robots" content="noindex">
 
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="apple-mobile-web-app-title" content="McCant's Boys Can">
+    <meta name="application-name" content="McCant's Boys Can">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+
     <title>The McCant's Boys Can</title>
 </head>
 <body>
